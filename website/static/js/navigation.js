f76969c346fb8a4dd381e9f1c0b54c2886b3f4c8
TTPO100AJIEX <ttpo100ajiex@mail.ru>
2023-04-07 18:50:49 +0300

    Website page setup

--- website/static/js/navigation.js
+++ website/static/js/navigation.js
@@ -1,23 +1,21 @@
 /*----------------------NAVIGATION MENU OPEN/CLOSE----------------------*/
 function open_mobile_navigation()
 {
-    document.getElementById("navigation").classList.toggle("mobile_hidden");
+    document.getElementById("navigation").classList.remove("closed");
+    document.getElementById("navigation_backdrop").classList.remove("closed");
+}
+function close_mobile_navigation()
+{
+    document.getElementById("navigation").classList.add("closed");
+    document.getElementById("navigation_backdrop").classList.add("closed");
 }
 
 Array.from(document.getElementsByClassName("navigation_opener")).forEach(button =>
 {
     button.addEventListener("click", open_mobile_navigation, { "capture": false, "once": false, "passive": true });
-})
-/*function open_control_nav()
+});
+Array.from(document.getElementsByClassName("navigation_closer")).forEach(button =>
 {
-    document.getElementById("control_nav").classList.remove("hidden");
-    document.getElementById("control_nav_backdrop").classList.remove("hidden");
-}
-function close_control_nav()
-{
-    document.getElementById("control_nav").classList.add("hidden");
-    document.getElementById("control_nav_backdrop").classList.add("hidden");
-}
-document.getElementById("control_nav_opener").addEventListener("click", open_control_nav, { "capture": false, "once": false, "passive": true });
-document.getElementById("control_nav_closer").addEventListener("click", close_control_nav, { "capture": false, "once": false, "passive": true });
-document.getElementById("control_nav_backdrop").addEventListener("click", close_control_nav, { "capture": false, "once": false, "passive": true });*/
+    button.addEventListener("click", close_mobile_navigation, { "capture": false, "once": false, "passive": true });
+});
+document.getElementById("navigation_backdrop").addEventListener("click", close_mobile_navigation, { "capture": false, "once": false, "passive": true });
