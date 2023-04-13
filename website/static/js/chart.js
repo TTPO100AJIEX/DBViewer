615338e7d441e1bb75c53ef56c29e15b0a7d61d7
Александр Абрамов <ttpo100ajiex@mail.ru>
2023-04-13 23:24:22 +0300

    Setup the table, minor modifications

--- website/static/js/chart.js
+++ website/static/js/chart.js
@@ -1,24 +1,13 @@
 export default class Chart
 {
     static #load = new Promise(resolve => Chart.onLoad = resolve);
+    
     constructor(elementId, records, update_interval, title, vAxisTitle, labels = { })
     {
         this.records = records;
         this.update_interval = update_interval;
-        this.title = title;
-        this.vAxisTitle = vAxisTitle;
         this.labels = labels;
-        Chart.#load.then(this.#drawChart.bind(this, elementId));
-        window.addEventListener("resize", this.#redraw.bind(this))
-    }
 
-    #drawnResolve;
-    #drawn = new Promise(resolve => this.#drawnResolve = resolve);
-    #options;
-    #data;
-    #chart;
-    async #drawChart(elementId)
-    {
         const styles = getComputedStyle(document.body);
         const colors =
         {
@@ -38,24 +27,36 @@ export default class Chart
             colors: [ colors.red, colors.yellow, colors.purple ],
             hAxis:
             {
-                format: 'hh:mm:ss', textStyle: { color: colors.text_minor },
+                format: 'hh:mm:ss', textStyle: { color: colors.text_minor }, slantedText: true,
                 gridlines: { color: colors.border }, minorGridlines: { color: colors.shadow },
                 title: "Время", titleTextStyle: { fontSize: 18, color: colors.text_brand },
                 minValue: new Date(Date.now() - this.records * this.update_interval),
-                slantedText: true
             },
-            interpolateNulls: true,
-            legend: { textStyle: { color: colors.text_major } },
-            lineWidth: 3, pointSize: 0, selectionMode: 'multiple',
-            title: this.title, titleTextStyle: { fontSize: 21, color: colors.text_major },
-            tooltip: { textStyle: { fontSize: 16, fontName: 'unset' } },
+            interpolateNulls: true, lineWidth: 3, pointSize: 0, selectionMode: 'multiple',
+            legend: { textStyle: { color: colors.text_major } }, tooltip: { textStyle: { fontName: 'unset' } },
+            title: title, titleTextStyle: { fontSize: 21, color: colors.text_major },
             vAxis:
             {
-                textStyle: { color: colors.text_minor },
-                gridlines: { color: colors.border }, minorGridlines: { color: colors.shadow },
-                title: this.vAxisTitle, titleTextStyle: { fontSize: 18, color: colors.text_brand }
+                textStyle: { color: colors.text_minor }, title: vAxisTitle, titleTextStyle: { fontSize: 18, color: colors.text_brand },
+                gridlines: { color: colors.border }, minorGridlines: { color: colors.shadow }
             }
         };
+        this.#adjustFontSize();
+        window.addEventListener("resize", () => { this.#adjustFontSize(); this.#redraw(); });
+        Chart.#load.then(this.#drawChart.bind(this, elementId));
+    }
+    async #adjustFontSize()
+    {
+        const defaultFontSize = Number(getComputedStyle(document.body).getPropertyValue("font-size").slice(0, -2));
+        this.#options.fontSize = defaultFontSize - 2;
+        this.#options.hAxis.titleTextStyle.fontSize = this.#options.vAxis.titleTextStyle.fontSize = defaultFontSize;
+        this.#options.titleTextStyle.fontSize = defaultFontSize + 2;
+    }
+
+    #drawnResolve; #drawn = new Promise(resolve => this.#drawnResolve = resolve);
+    #options; #data; #chart;
+    async #drawChart(elementId)
+    {
         this.#data = new google.visualization.DataTable({ cols: [ { type: 'datetime', label: 'date' } ] });
         this.#chart = new google.visualization.LineChart(document.getElementById(elementId));
         this.#drawnResolve();
@@ -68,13 +69,19 @@ export default class Chart
         await this.#drawn;
 
         Object.keys(record).filter(column => this.#data.getColumnIndex(column) == -1).forEach(column => this.#data.addColumn('number', this.labels[column] ?? column, column));
-        this.#options.hAxis.minValue = new Date(Date.now() - this.records * this.update_interval);
 
         let row = [ new Date(), ...new Array(this.#data.getNumberOfColumns() - 1) ];
         for (const column in record) row[this.#data.getColumnIndex(column)] = Number(record[column]);
         this.#data.addRow(row);
+        
+        if (this.#options.hAxis.minValue)
+        {
+            const timeEstimate = new Date(Date.now() - this.records * this.update_interval);
+            if (timeEstimate < this.#data.getValue(0, 0)) this.#options.hAxis.minValue = timeEstimate;
+            else delete this.#options.hAxis.minValue;
+        }
 
-        while (this.#data.getNumberOfRows() > 100) this.#data.removeRow(0);
+        while (this.#data.getNumberOfRows() > this.records) this.#data.removeRow(0);
         this.#redraw();
     }
 };
